bac7602fb6d37d62db3da341ef04f3c52054d1a0
Noah <noah.colbourne@ocsbstudent.ca>
2019-09-30 13:07:38 -0400

    Guess Number

---
+++ pizzacostnoah/pizzacostnoah/PizzaCostForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace pizzacostnoah
+{
+    partial class PizzaCostForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblnotaxanswer = new System.Windows.Forms.Label();
+            this.lbltax = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtdiameter = new System.Windows.Forms.TextBox();
+            this.lbltaxanswer = new System.Windows.Forms.Label();
+            this.lblnotax = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(0, 79);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(503, 29);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Enter the diameter of the pizza (in inches):";
+            this.lblQuestion.Click += new System.EventHandler(this.LblQuestion_Click);
+            // 
+            // lblnotaxanswer
+            // 
+            this.lblnotaxanswer.AutoSize = true;
+            this.lblnotaxanswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnotaxanswer.Location = new System.Drawing.Point(561, 308);
+            this.lblnotaxanswer.Name = "lblnotaxanswer";
+            this.lblnotaxanswer.Size = new System.Drawing.Size(59, 31);
+            this.lblnotaxanswer.TabIndex = 1;
+            this.lblnotaxanswer.Text = "???";
+            // 
+            // lbltax
+            // 
+            this.lbltax.AutoSize = true;
+            this.lbltax.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltax.Location = new System.Drawing.Point(26, 384);
+            this.lbltax.Name = "lbltax";
+            this.lbltax.Size = new System.Drawing.Size(271, 31);
+            this.lbltax.TabIndex = 2;
+            this.lbltax.Text = "The cost (with tax) is:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(361, 186);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(198, 54);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // txtdiameter
+            // 
+            this.txtdiameter.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdiameter.Location = new System.Drawing.Point(567, 79);
+            this.txtdiameter.Name = "txtdiameter";
+            this.txtdiameter.Size = new System.Drawing.Size(195, 40);
+            this.txtdiameter.TabIndex = 4;
+            // 
+            // lbltaxanswer
+            // 
+            this.lbltaxanswer.AutoSize = true;
+            this.lbltaxanswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltaxanswer.Location = new System.Drawing.Point(561, 384);
+            this.lbltaxanswer.Name = "lbltaxanswer";
+            this.lbltaxanswer.Size = new System.Drawing.Size(59, 31);
+            this.lbltaxanswer.TabIndex = 5;
+            this.lbltaxanswer.Text = "???";
+            // 
+            // lblnotax
+            // 
+            this.lblnotax.AutoSize = true;
+            this.lblnotax.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnotax.Location = new System.Drawing.Point(26, 308);
+            this.lblnotax.Name = "lblnotax";
+            this.lblnotax.Size = new System.Drawing.Size(309, 31);
+            this.lblnotax.TabIndex = 6;
+            this.lblnotax.Text = "The cost (without tax) is:";
+            // 
+            // PizzaCostForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblnotax);
+            this.Controls.Add(this.lbltaxanswer);
+            this.Controls.Add(this.txtdiameter);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lbltax);
+            this.Controls.Add(this.lblnotaxanswer);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "PizzaCostForm";
+            this.Text = "Pizza Cost By Noah.C";
+            this.Load += new System.EventHandler(this.PizzaCostForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblnotaxanswer;
+        private System.Windows.Forms.Label lbltax;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtdiameter;
+        private System.Windows.Forms.Label lbltaxanswer;
+        private System.Windows.Forms.Label lblnotax;
+    }
+}
+
---
+++ pizzacostnoah/pizzacostnoah/PizzaCostForm.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Created by: Noah Colbourne
+ * Created on: 26-Sep-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #11 - Name of Program
+ * This program will calculate the cost (before and after tax) 
+ * of a pizza based on the diameter of the pizza. 
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pizzacostnoah
+{
+    public partial class PizzaCostForm : Form
+    {
+        public PizzaCostForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PizzaCostForm_Load(object sender, EventArgs e)
+        {
+            //This will hide the the questions for the answers and the answers.
+            this.lblnotax.Hide();
+            this.lbltax.Hide();
+            this.lblnotaxanswer.Hide();
+            this.lbltaxanswer.Hide();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            //This will show the the questions for the answers and the answers.
+            this.lblnotax.Show();
+            this.lbltax.Show();
+            this.lblnotaxanswer.Show();
+            this.lbltaxanswer.Show();
+
+            //This declares these variables as doubles.
+            double diameter, costbeforetax, costaftertax;
+
+            // This makes the variable diameter become the text box.
+            diameter = double.Parse(txtdiameter.Text);
+
+            //this makes the variable costbeforetx become the equation for price without tax.
+            costbeforetax = (0.75 + 0.99 + (0.5 * diameter));
+
+            //This makes the varirbale costaftertax become the equation for price with tax.
+            costaftertax = ((0.75 + 0.99 + 0.5 * diameter)*1.13);
+
+            //This makes the answer label for cost without tax display the answer for cost without tax.
+            this.lblnotaxanswer.Text = String.Format("${0:0.00}", costbeforetax);
+
+            //This makes the answer label for cost with tax display the answer for cost with tax.
+            this.lbltaxanswer.Text = String.Format("${0:0.00}", costaftertax);
+
+        }
+
+        private void LblQuestion_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
